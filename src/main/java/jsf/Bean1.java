d49a7ede91490d9bc288ced2c181c1c644d8992b
USER <USER@DESKTOP-EGDNFSG>
2023-11-05 17:10:13 +0100

    TP3 exercice 4

--- src/main/java/jsf/Bean1.java
+++ src/main/java/jsf/Bean1.java
@@ -37,6 +37,6 @@ public class Bean1 {
         return l;
     }
     public String afficher() {
-    return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
+    return "affichage__4?nb="+ nombre + "&amp;faces-redirect=true";
     }
 }
